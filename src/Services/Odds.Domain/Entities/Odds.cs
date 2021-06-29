6714ecb8c90d2839aff85e5bdd1a354b0a911a17
Abenezer Bayu <abayu@excellerentsolutions.com>
2021-06-29 13:38:14 +0300

    Initial Project Structure.

---
+++ src/Services/Odds.Domain/Entities/Odds.cs
@@ -0,0 +1,12 @@
+﻿using Odds.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Odds.Domain.Entities
+{
+    public class Odds : EntityBase
+    {
+
+    }
+}
